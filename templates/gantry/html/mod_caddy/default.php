b85cd2daa14babaaf40e77818a91214fef04f30b
womantoys <womantoys@email.ru>
2014-08-30 18:42:48 +0400

    Add mod_caddy template

---
+++ templates/gantry/html/mod_caddy/default.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package		Ishop.Site
+ * @subpackage	mod_caddy
+ * @copyright	Copyright (C) 2010 - 2013 Konstantin Ovcharenko.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+?>
+<div id="mod_caddy_ishop">
+    <form 
+        action="<?php echo JRoute::_('index.php'); ?>" 
+        method="post" 
+        name="adminForm"
+        class="btn-header"
+        id="adminForm"
+    >
+        <dl>
+            <span id="mod_caddy_product_sum"><?php echo $caddy_data['sum']?></span>
+            <span class="rub"><?php echo JText::_('MOD_CADDY_RUB')?></span>
+            <span class="t">товаров:</span>
+            <span id="mod_caddy_product_count"><?php echo $caddy_data['count']?></span> <span class="t">шт.</span>
+        </dl>
+        <input 
+            type="submit" 
+            value=""
+            id="btn-cart"
+        >
+
+        <input type="hidden" name="option" value="com_ishop" />
+        <input type="hidden" name="view" value="caddy" />
+        <?php echo JHtml::_('form.token'); ?>
+    </form>    
+</div>
